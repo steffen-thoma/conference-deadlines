12c1f687a579f9cfa02b8bcf02f23b9999f7365e
Alex Naumann <alexander@naumail.de>
2022-08-07 16:14:54 +0200

    feat: adjust update_masterdata.py

--- python/conversion/update_masterdata.py
+++ python/conversion/update_masterdata.py
@@ -1,31 +1,34 @@
-from pathlib import Path
-
-from python.config import csv_path_master_data, csv_path_conferences
-from python.io import load_csv, save_csv
-
-project_root = Path(__file__).parent.parent.parent
+from python.config import csv_path_master_data, yaml_path_conferences
+from python.io import load_csv, save_csv, load_yaml
 
 
 def update_master_data_from_conferences():
-    data = load_csv(csv_path_conferences)
+    conferences_data = load_yaml(yaml_path_conferences)
     master_data = load_csv(csv_path_master_data)
 
-    titles = set([r['title'] for r in data])
+    titles = set([r["title"] for r in conferences_data])
+    master_data_conference_titles = list(set([c["title"].lower() for c in master_data]))
     for title in titles:
-        confs = [r for r in data if r['title'] == title]  # find matching title
-        conf = sorted(confs, key=lambda x: int(x['year']), reverse=True)[0]  # use newest conf entry for update
-        if conf['title'].lower() not in list(set([c['title'].lower() for c in master_data])):
-            master_data.append({
-                'title': conf['title'].lower(),
-                'full_name': conf['full_name'],
-                'wikicpf_query': conf['title'],
-                'wikicpf_link': "",
-                'ranking': conf['ranking'],
-                'hindex': conf['hindex'],
-                'sub': conf['sub'],
-            })
-    save_csv(csv_path_master_data, master_data)
+        # find matching title
+        confs = [r for r in conferences_data if r["title"] == title]
+        # use newest conf entry for update
+        conf = sorted(confs, key=lambda x: int(x["year"]), reverse=True)[0]
+        if conf["title"].lower() not in master_data_conference_titles:
+            master_data.append(
+                {
+                    "title": conf.get("title", "").lower(),
+                    "full_name": conf.get("full_name", ""),
+                    "wikicfp_query": conf.get("title", ""),
+                    "wikicfp_link": "",
+                    "sub": conf.get("sub", ""),
+                }
+            )
+    save_csv(
+        csv_path_master_data,
+        master_data,
+        key_order=["title", "full_name", "wikicfp_query", "wikicfp_link", "sub"],
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     update_master_data_from_conferences()
